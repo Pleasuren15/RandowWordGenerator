6b2752eb92f2a46eab558bcd8a58f887c91021f3
Pleasuren15 <77783514+Pleasuren15@users.noreply.github.com>
2021-06-03 11:10:25 +0200

    RandomWordsViewModel(NumberOfWords, FirstLetter, LastLetter, Paragraphs, MinimumLength, MaximumLength) model added with listed properties in brackets

---
+++ Models/RandomWordsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RandowWordGenerator.Models
+{
+    public class RandomWordsViewModel
+    {
+        [Required]
+        [DisplayName("Words")]
+        public int NumberOfOWords { get; set; }
+
+        [Required]
+        [DisplayName("First Letter")]
+        public string FirstLetter { get; set; } 
+
+        [Required]
+        [DisplayName("Last Letter")]
+        public string LastLetter { get; set; }  
+        
+        [Required]
+        [DisplayName("Paragraph(s)")]
+        public string Paragraphs { get; set; }
+
+        [DisplayName("Minimum")]
+        [Required]
+        public int MinimumWordLength { get; set; }
+        
+        [Required]
+        [DisplayName("Maximum")]
+        public int MaximumWordLength { get; set; }
+    }
+}
